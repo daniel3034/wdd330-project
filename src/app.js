7c77926e43b1ae6d25bd6204cfa555ab6e66aa72
daniel3034 <danielh3034@gmail.com>
2025-04-04 19:25:37 -0500

    1

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 
-app.use(express.static(path.join(__dirname, 'src/public')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
+app.use(express.static('src/public'));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+//   });
 module.exports = app;
