c1fd5757a45470ccc77f708d2f976f72cf7faf63
daniel3034 <danielh3034@gmail.com>
2025-04-05 22:13:02 -0500

    .

--- src/app.js
+++ src/app.js
@@ -1,17 +1,5 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-// Serve static files from the "src/public" directory
-app.use(express.static(path.join(__dirname, 'src/public')));
-
-// Serve the main HTML file
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/public/index.html'));
-});
-
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.use(express.static('src/public'));
+module.exports = app;
